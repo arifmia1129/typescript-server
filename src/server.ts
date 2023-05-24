7c3a2aaa7ef697477c36432a403507cbe7f03815
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-24 08:01:32 +0600

    typescript server initial commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port:number = 3000
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mongoose');
+
+//   listing app
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+  })
+}
+
+
+
+
+
+
+
+
+
+
